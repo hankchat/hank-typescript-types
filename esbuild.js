3648b009d3af5ab8bc73adc63c1f1825e8eb2f2f
Marc Addeo <marcaddeo@gmail.com>
2024-09-24 13:16:12 -0400

    Switch to esbuild

---
+++ esbuild.js
@@ -0,0 +1,14 @@
+const esbuild = require("esbuild");
+
+esbuild
+  .build({
+    entryPoints: [
+      "src/**/*.ts"
+    ],
+    outdir: "dist",
+    bundle: true,
+    sourcemap: true,
+    minify: false, // might want to use true for production build
+    format: "cjs", // needs to be CJS for now
+    target: ["es2020"] // don't go over es2020 because quickjs doesn't support it
+  })
