0e036f2084a499a2f623fbcf57860d6dec260e81
Marc Addeo <marcaddeo@gmail.com>
2024-09-24 10:05:17 -0400

    Only compile types to CJS

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/**/*.ts"],
-  format: ["cjs", "esm"],
+  format: ["cjs"],
   dts: true,
   splitting: false,
   sourcemap: true,
