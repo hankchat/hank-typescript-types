cb898f03e27d8d71dbda90ca496fd9f23c3b1499
Marc Addeo <marcaddeo@gmail.com>
2024-09-26 12:26:19 -0400

    Start custom ser/de

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-export * from "./access_check/access_check";
+import { BinaryReader, BinaryWriter } from "@bufbuild/protobuf/wire";
+import { type AccessCheckChain as AccessCheckChainType, AccessCheckChain as GeneratedAccessCheckChain } from "./access_check/access_check";
+
+export { AccessCheck } from "./access_check/access_check";
 export * from "./access_check/access_check_operator";
 export * from "./cron/cron_job";
 export * from "./cron/one_shot_job";
@@ -18,3 +21,55 @@ export * from "./message/message";
 export * from "./message/reaction";
 export * from "./plugin/metadata";
 export * from "./hank";
+
+export const AccessCheckChain: MessageFns<AccessCheckChainType> = {
+  ...GeneratedAccessCheckChain,
+
+  fromJSON(object: any): AccessCheckChainType {
+    console.log(object);
+    return {
+      operator: -1,
+      checks: [],
+    };
+  },
+};
+
+type Builtin = Date | Function | Uint8Array | string | number | boolean | undefined;
+
+type DeepPartial<T> = T extends Builtin ? T
+  : T extends globalThis.Array<infer U> ? globalThis.Array<DeepPartial<U>>
+  : T extends ReadonlyArray<infer U> ? ReadonlyArray<DeepPartial<U>>
+  : T extends { $case: string; value: unknown } ? { $case: T["$case"]; value?: DeepPartial<T["value"]> }
+  : T extends {} ? { [K in keyof T]?: DeepPartial<T[K]> }
+  : Partial<T>;
+
+type KeysOfUnion<T> = T extends T ? keyof T : never;
+type Exact<P, I extends P> = P extends Builtin ? P
+  : P & { [K in keyof P]: Exact<P[K], I[K]> } & { [K in Exclude<keyof I, KeysOfUnion<P>>]: never };
+
+interface MessageFns<T> {
+  encode(message: T, writer?: BinaryWriter): BinaryWriter;
+  decode(input: BinaryReader | Uint8Array, length?: number): T;
+  fromJSON(object: any): T;
+  toJSON(message: T): unknown;
+  create<I extends Exact<DeepPartial<T>, I>>(base?: I): T;
+  fromPartial<I extends Exact<DeepPartial<T>, I>>(object: I): T;
+}
+
+// Testing
+
+let jsonString = `
+{
+  "OR": [
+    {
+      "user": "1231231213"
+    },
+    {
+      "role": "OGs"
+    }
+  ]
+}`;
+
+let chain2 = AccessCheckChain.fromJSON(JSON.parse(jsonString));
+console.log(chain2);
+
