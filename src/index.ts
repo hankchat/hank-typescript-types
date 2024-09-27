db25bab2e25ac36ff1322d5246e3257374bb51b7
Marc Addeo <marcaddeo@gmail.com>
2024-09-26 20:35:42 -0400

    Add generated types

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,26 @@ import { type AccessCheckChain as AccessCheckChainType, AccessCheckChain as Gene
 import { accessCheckOperatorFromJSON } from "./access_check/access_check_operator";
 
 // @@proto-exports-begin
+export * from "./access_check/access_check_operator";
+export * from "./database/prepared_statement";
+export * from "./access_check/access_check";
+export * from "./io/send_message_output";
+export * from "./io/send_message_input";
+export * from "./io/one_shot_output";
+export * from "./io/db_query_output";
+export * from "./io/one_shot_input";
+export * from "./io/db_query_input";
+export * from "./cron/one_shot_job";
+export * from "./message/reaction";
+export * from "./database/results";
+export * from "./plugin/metadata";
+export * from "./message/message";
+export * from "./io/react_output";
+export * from "./io/react_input";
+export * from "./io/cron_output";
+export * from "./io/cron_input";
+export * from "./cron/cron_job";
+export * from "./hank";
 // @@proto-exports-end
 
 // Override the json ser/de for AccessCheckChain to use our custom format.
